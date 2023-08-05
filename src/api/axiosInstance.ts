62d07b6d579a7dd72013779c08badef74946bab6
alatoaku <alatoaku@gmail.com>
2023-08-05 15:24:12 +0900

    Code optimized

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://loader.to/ajax',
-  timeout: 5000,
+  timeout: 500,
   headers: {
     'Content-Type': 'application/json',
-  },
+  }
 });
 
 export default axiosInstance;
